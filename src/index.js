c7f5762f541c186e3e429b3af672dd0218f13d6d
ceimiplace <azamfiredenis@yahoo.com>
2022-12-20 12:09:09 +0200

    starter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+const appInfo = {
+  title: "Indecision app",
+  subtitle: "This is some app info",
+  options: ["one", "two"],
+};
+let template = (
+  <div>
+    <h1>{appInfo.title}</h1>
+    {appInfo.subtitle && <p>{appInfo.subtitle}</p>}
+    <ol>
+      <li>LIST ITEM,1</li>
+      <li>LIST ITEM,2</li>
+    </ol>
+    <p>
+      There are{" "}
+      {appInfo.options.length > 0
+        ? appInfo.options.length === 1
+          ? "one option"
+          : `${appInfo.options.length} options`
+        : "no"}
+    </p>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user = { name: "Azamfire Denis", age: 28, location: "Botosnai" };
+let challenge = (
+  <div>
+    <h1>{user.name}</h1>
+    {user.age && <p>Age- {user.age}</p>}
+    <p>Location - {user.location}</p>
+  </div>
+);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(template);
